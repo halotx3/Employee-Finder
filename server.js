f68d0642d449b98d372a86f35c06034c19095212
Christopher Brownlee <cdbreach@gmail.com>
2019-04-11 17:46:00 -0400

    Fixed routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'./app/public')));
 
 require('./app/public/routes/apiRoutes.js')(app);
 require('./app/public/routes/htmlRoutes.js')(app);
